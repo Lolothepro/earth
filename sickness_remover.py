2067af5975a9e84430de3cae94e566f9c56ae373
Lolothepro <68275908+Lolothepro@users.noreply.github.com>
2022-02-20 08:50:03 +0100

    Update sickness_remover.py

--- sickness_remover.py
+++ sickness_remover.py
@@ -7,8 +7,8 @@ for human in humans:
     human.has_antibodies = True
     human.is_in_love = False
     human.is_stalker = False
-if prob_two_human = <25%:
-    prob_two_human = 0%
+if prob_two_human = <0.25:
+    prob_two_human = 0
     
     
 for sickness in sicknesses:
